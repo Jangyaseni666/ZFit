9302c248cce3060747de7863d186b7e36c499f28
Jangyaseni666 <jangyasenisahoo17@gmail.com>
2023-07-26 07:23:39 +0530

    Creating empty files

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+import {ChakraProvider} from '@chakra-ui/react';
+function App() {
+  return (
+    <ChakraProvider>
+      {/* Your code */}
+    </ChakraProvider>
+  );
+}
+
+export default App;
